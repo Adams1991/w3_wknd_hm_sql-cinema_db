5c74096872d781dcbabeea302c55b36bac066280
Adams1991 <adams121@hotmail.co.uk>
2018-06-08 12:33:35 +0100

    console set up for basic exntensions and fix to change film price to integer

--- db/console.rb
+++ db/console.rb
@@ -9,16 +9,24 @@ Film.delete_all()
 Ticket.delete_all()
 
 customer1 = Customer.new({ 'name' => 'Shaun', 'funds' => 10 })
+customer2 = Customer.new({ 'name' => 'Rachel', 'funds' => 12 })
 
 customer1.save()
+customer2.save()
 
 film1 = Film.new({ 'title' => 'Deadpool', 'price' => 10 })
+film2 = Film.new({ 'title' => 'Grease', 'price' => 12 })
 
 film1.save()
+film2.save()
 
 ticket1 = Ticket.new({ 'film_id' => film1.id, 'customer_id' => customer1.id })
+ticket2 = Ticket.new({ 'film_id' => film1.id, 'customer_id' => customer1.id })
+ticket3 = Ticket.new({ 'film_id' => film1.id, 'customer_id' => customer2.id })
 
 ticket1.save()
+ticket2.save()
+ticket3.save()
 
 binding.pry
 nil
