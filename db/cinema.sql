790d10d4558477d974084aa3dde7353f0235b498
Adams1991 <adams121@hotmail.co.uk>
2018-06-09 16:23:34 +0100

    screenings table in sql updated for available tickets

--- db/cinema.sql
+++ db/cinema.sql
@@ -19,7 +19,8 @@ CREATE TABLE films (
 CREATE TABLE screenings (
   id SERIAL4 PRIMARY KEY,
   screening_time VARCHAR(255),
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  available_tickets INT4
 );
 
 CREATE TABLE tickets (
